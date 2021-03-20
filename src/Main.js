08ffdff6eb4f29a3ed6537a173e239cf83a8d255
Tyler Johnson <tyler@csdconsultants.us>
2021-03-20 16:48:16 -0700

    created header, main, and footer components each with a JS and CSS file and linked to app.js

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className= 'main'>This is the Main</div>
+    );
+  }
+}
+
+
+export default Main;
