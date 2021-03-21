35ea11c12151d0f6b736ceca948c533ded47dc8c
Tyler Johnson <tyler@csdconsultants.us>
2021-03-20 18:14:59 -0700

    added hornedbeasts component and added to main component using props

---
+++ src/HornedBeasts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+
+  render() {
+    return (
+      <>
+      <h2>{this.props.title}</h2>
+      <img src={this.props.imageUrl} alt="BeastOne" />
+      <p>{this.props.description}</p>
+      </>
+    )
+  };
+}
+
+export default HornedBeasts;
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Main.css';
+import HornedBeasts from './HornedBeasts.js';
+
 
 class Main extends React.Component {
   render() {
     return (
-      <div className= 'main'>This is the Main</div>
+      <>
+      <HornedBeasts title={'TitleOne'} imageUrl={'https://images.unsplash.com/photo-1518946222227-364f22132616?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'} description={'BeastOne'} />
+      <HornedBeasts title={'TitleTwo'} imageUrl={'https://images.unsplash.com/photo-1541414779316-956a5084c0d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=696&q=80'} description={'BeastTwo'}/>
+      </>
     );
   }
 }
