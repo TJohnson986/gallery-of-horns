498c69f477f2bb987f22cc719f9c57c1f189bb32
Tyler Johnson <tyler@csdconsultants.us>
2021-03-27 15:20:09 -0700

    created beastArray set to data.map, removed placeholder images and text for code that iterates through data.json

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './Main.css';
 import HornedBeasts from './HornedBeasts.js';
-
+import data from './data.json';
 
 class Main extends React.Component {
   render() {
-    return (
-      <>
-      <HornedBeasts title={'TitleOne'} imageUrl={'https://images.unsplash.com/photo-1518946222227-364f22132616?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'} description={'BeastOne'} />
-      <HornedBeasts title={'TitleTwo'} imageUrl={'https://images.unsplash.com/photo-1541414779316-956a5084c0d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=696&q=80'} description={'BeastTwo'}/>
-      </>
-    );
-  }
-}
+    let beastArray = data.map( animal => {
+    return <HornedBeasts 
+        name = {animal.keyword}
+        title = {animal.title}
+        image_url = {animal.image_url}
+        description = {animal.description}
+          
+          />
+        });
+        
+        return (
+          <main>
+          {beastArray}
+          </main>
+        );
 
+    }
+  }
 
 export default Main;
