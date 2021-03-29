34802622bad2920c41914bb823227dfe2dae5c7f
Tyler Johnson <tyler@csdconsultants.us>
2021-03-29 15:40:54 -0700

    added minor styling changes to resize pictures, add background and button colors. Changed button styleand added heart emoji.

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 class HornedBeasts extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numberOfHearts: 0
+    }
+  }
+
+  buttonClicked = () => {
+    this.setState({
+      numberOfHearts: this.state.numberOfHearts + 1
+    });
+  }
 
   render() {
     return (
       <>
-      <h2>{this.props.title}</h2>
-      <img src={this.props.imageUrl} alt="BeastOne" />
-      <p>{this.props.description}</p>
+        <h2>{this.props.title}</h2>
+        <img onClick={this.buttonClicked} src={this.props.image_url} alt={this.props.keyword} title={this.props.title} />
+        <div>🖤{this.state.numberOfHearts} </div>
+        <p>{this.props.description}</p>
+        <Button onClick={this.buttonClicked} variant="Primary" size="lg" className="button">This is my fav!</Button>
       </>
     )
   };
