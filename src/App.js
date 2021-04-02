1068514cb493f4fca0e636ae4d8c5c203b4b2139
Tyler Johnson <tyler@csdconsultants.us>
2021-04-01 20:55:17 -0700

    added show and hide image methods

--- src/App.js
+++ src/App.js
@@ -4,18 +4,48 @@ import FormFilter from './FormFilter.js';
 import Header from './Header.js';
 import Main from './Main.js';
 import React from 'react';
+import SelectedBeast from './SelectedBeast.js';
 import data from './data.json';
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      show: false,
+      animalImage: {}
+     }
+   }
+
+  showImageInModal = clickedImage => {
+    this.setState ({
+      show: true,
+      animalImage: clickedImage,
+    })
+  }
+
+  hideImageInModal = () => {
+    this.setState ({
+      show: false,
+      animalImage: {},
+    })
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <FormFilter />
-          animals = {data};
-        <Main />
-          animals = {data};
+        <FormFilter 
+          animals = {data}
+          />;
+        <SelectedBeast 
+          show={this.state.show}
+          hideImage={this.hideImageInModal}
+          animalImage={this.state.animalImage} />
+        <Main 
+          animals = {data}
+          handleClick={this.showImageInModal}
+          />;
         <Footer />
       </div>
     );
